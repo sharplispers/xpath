aa6da7b0b7c35502f05bb65dc7c586e4883a8fed
Cyrus Harmon <ch-github@bobobeach.com>
2018-09-05 10:59:31 -0700

    move xpath-test stuff to its own package

--- xnum-ieee-test.lisp
+++ xnum-ieee-test.lisp
@@ -27,7 +27,7 @@
 ;;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 ;;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-(in-package :xpath)
+(in-package :xpath-test)
 
 ;; TODO: converting to string
 (deftest test-xnum
